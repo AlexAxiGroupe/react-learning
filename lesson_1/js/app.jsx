65d91be4b10694a49915ca621fcd96e6355203ce
Alexander Plakhotenko <axi.group.alex@gmail.com>
2017-03-14 22:23:10 +0200

    react

---
+++ lesson_1/js/app.jsx
@@ -0,0 +1,7 @@
+var jsx =
+<div className="container">
+	<img src="img/logo.png" alt=""/>
+	<h1>Hello, React</h1>
+	<p>My first React application</p>
+</div>;
+ReactDOM.render(jsx, document.getElementById('root'));
