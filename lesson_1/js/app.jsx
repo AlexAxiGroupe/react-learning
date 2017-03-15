91d9b75081e4a486782cb02ffefc3f4f2346c671
Alexander Plakhotenko <axi.group.alex@gmail.com>
2017-03-15 18:40:20 +0200

    lesson 5

--- lesson_1/js/app.jsx
+++ lesson_1/js/app.jsx
@@ -1,7 +1,11 @@
-var jsx =
-<div className="container">
-	<img src="img/logo.png" alt=""/>
-	<h1>Hello, React</h1>
-	<p>My first React application</p>
-</div>;
-ReactDOM.render(jsx, document.getElementById('root'));
+function Hero(){
+	return(
+	<div className="container">
+		<img src="img/logo.png" alt=""/>
+		<h1>React</h1>
+		<p>My first React application</p>
+	</div>
+	);
+}
+
+ReactDOM.render(<Hero />, document.getElementById('root'));
